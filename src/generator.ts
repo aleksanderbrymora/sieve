766e03d54f81a0445632dceeadccfd22f3f0e983
aleksanderbrymora <aleksander.australia@gmail.com>
2020-08-18 12:19:07 +1000

    added some comments to generator

--- src/generator.ts
+++ src/generator.ts
@@ -10,7 +10,11 @@ const sieve = (limit: number): number[] => {
 	let range: number[] = [...Array(limit + 1).keys()].slice(2);
 	for (let i = 2; i <= Math.sqrt(limit); i++) {
 		const toFilter: number[] = [];
+		// generator functions also have an iterator inside of them, so we can use for of to generate the array
+		// great vid to understand whats happening https://www.youtube.com/watch?v=EzdgkEMvrvA
 		for (const n of createRangeGenerator(i, limit)) toFilter.push(n);
+		// got this from here https://stackoverflow.com/a/33034768/12654572
+		// Its to get the difference between the arrays
 		range = range.filter((x) => !toFilter.includes(x));
 	}
 	return range;
